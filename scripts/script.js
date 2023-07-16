97daaa7483631b3e65ad8b7ab8c561f5a39c2a76
Pavlo Nechai <nechaipavel7@gmail.com>
2023-07-17 00:06:31 +0300

    mob menu

--- scripts/script.js
+++ scripts/script.js
@@ -46,6 +46,24 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 // ---------------------
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".mobile_menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+    n.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    })
+);
+
+
+
+// ----------------------
 
 document.addEventListener('DOMContentLoaded', function() {
     const jewelleryMenuItemMob = document.querySelector('.menu_item-mob:first-child');
